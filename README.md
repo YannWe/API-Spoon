0c8d2da304f00e85895931868ec66131d1b13c1f
ywe <janno4@gmail.com>
2022-08-23 12:00:20 +0200

    saving testing program

---
+++ README.md
@@ -0,0 +1,6 @@
+*** Research Results  ***
+
+Requirements:
+1. We need to be able to search by a large list of ingredients according to a users pre-defined items they have available. The returned results should display the ingredients that are available as well as the ingredients that are missing (from users pre-defined list).
+2. With the result of the ingredient search the user should be able to choose a recipe. Once the users clicks on the recipe, the API should provide us with an ID. With the ID we can request the recipe data/details (including instructions/steps) and return them to the user
+3. 
---
+++ server.js
@@ -0,0 +1,57 @@
+import express from "express";
+import axios from "axios";
+import "dotenv/config";
+const app = express();
+const PORT = process.env.PORT;
+const API_KEY = process.env.API_KEY;
+
+//Get recipe by ingredients
+app.get('/recipes/:query', async (req, res) => {
+    const response = await axios.get(
+        `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${req.params.query}&number=2&apiKey=${API_KEY}`
+    );
+    const data = response.data;
+        console.log(data.length)
+    
+    const availableIngredients = data.map(items => {
+        items.usedIngredients.map(item => {console.log(item.name)})
+    });
+    const missingIngredients = data.map(items => {
+        items.missedIngredients.map(item => {console.log(item.name)})
+    });
+    res.json(data)
+});  
+
+// get recipe information by ID
+app.get('/recipes/getrecipe/:id', async (req, res) => {
+    const response = await axios.get(
+        `https://api.spoonacular.com/recipes/${req.params.id}/information?&apiKey=${API_KEY}`
+    );
+    const data = response.data;
+        console.log(data)
+    res.json(data)
+});  
+
+// get recipe by complex search - beware: this is an exact search - if 1 ingredient is missing from the recipe, no results will appear
+app.get('/recipes/complex/:query', async (req, res) => {
+    const response = await axios.get(
+        `https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${req.params.query}&diet="vegetarian"&apiKey=${API_KEY}`
+    );
+    const data = response.data;
+        console.log(data)
+    res.json(data)
+}); 
+
+// get specific recipes instruction/steps
+app.get('/recipes/instructions/:id', async (req, res) => {
+    const response = await axios.get(
+        `https://api.spoonacular.com/recipes/${req.params.id}/analyzedInstructions?&apiKey=${API_KEY}`
+    );
+    const data = response.data;
+        console.log(data)
+    res.json(data)
+}); 
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
